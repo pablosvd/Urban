67a12e1125d1448fbde0d327c1b3b11e68a9201a
pablosvd <75953063+pablosvd@users.noreply.github.com>
2024-07-28 21:27:45 +0300

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -1,14 +1,25 @@
-def send_email(message, recipient, sender = 'university.help@gmail.com'):
+def send_email(message, recipient, sender='university.help@gmail.com'):
+    count = 0
+    domen = ['.com', '.net', '.ru']
+
     if recipient == sender:
         print('Нельзя отправить письмо самому себе!')
-    elif recipient != sender:
-        count = 0
-        dot = ['.com', '.net', '.ru']
-        for i in dot:
-            if i in recipient and sender:
+    else:
+        for i in domen:
+            if i in sender:
                 count = 0 + 1
-        if count > 0 :
-            print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
-        else:  print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
 
-send_email('Привет', 'pavel@gmail.com')
+        if count > 0:
+
+            if sender == 'university.help@gmail.com':
+                print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+            elif sender != 'university.help@gmail.com':
+                print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+        else:
+            print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
